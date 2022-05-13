d71db21147ec559da39bafb94049df1b6ecf51c6
Alexander Kharichev <myx@myx.ru>
2022-05-13 10:50:34 +0300

    * source: keywork list formatting

--- java/ru/myx/ae3/eval/parse/ExpressionParser.java
+++ java/ru/myx/ae3/eval/parse/ExpressionParser.java
@@ -147,11 +147,63 @@ public final class ExpressionParser {
 	}
 	
 	static {
-		for (final String key : new String[]{
-				"break", "else", "new", "var", "case", "finally", "return", "void", "catch", "for", "switch", "while", "continue", "function", "this", "with", "default", "if",
-				"throw", "delete", "in", "try", "do", "instanceof", "typeof", "abstract", "enum", "int", "short", "boolean", "export", "interface", "static", "byte", "extends",
-				"long", "super", "char", "final", "native", "synchronized", "class", "float", "package", "throws", "const", "goto", "private", "transient", "debugger",
-				"implements", "protected", "volatile", "double", "import", "public"
+		for (final String key : new String[]{ //
+				"abstract", //
+				"break", //
+				"boolean", //
+				"byte", //
+				"case", //
+				"catch", //
+				"char", //
+				"class", //
+				"continue", //
+				"const", //
+				"debugger", //
+				"default", //
+				"delete", //
+				"do", //
+				"double", //
+				"else", //
+				"enum", //
+				"export", //
+				"extends", //
+				"for", //
+				"function", //
+				"final", //
+				"float", //
+				"finally", //
+				"goto", //
+				"interface", //
+				"instanceof", //
+				"implements", //
+				"import", //
+				"int", //
+				"if", //
+				"in", //
+				"long", //
+				"new", //
+				"native", //
+				"package", //
+				"private", //
+				"protected", //
+				"public", //
+				"return", //
+				"short", //
+				"static", //
+				"super", //
+				"switch", //
+				"synchronized", //
+				"throws", //
+				"this", //
+				"throw", //
+				"try", //
+				"typeof", //
+				"transient", //
+				"var", //
+				"void", //
+				"volatile", //
+				"while", //
+				"with", //
 		}) {
 			ExpressionParser.DEFAULT_VARIABLES.put(key, new TKV_LCONST(ExpressionParser.RESERVED_OBJECT, key));
 		}
