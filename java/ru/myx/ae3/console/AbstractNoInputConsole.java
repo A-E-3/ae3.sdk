4c20f0e3f4ba76570eee48c6c57a257e17ae620b
Alexander Kharichev <myx@myx.ru>
2022-12-27 20:55:36 +0200

    * minor: source formatting

--- java/ru/myx/ae3/console/AbstractNoInputConsole.java
+++ java/ru/myx/ae3/console/AbstractNoInputConsole.java
@@ -8,89 +8,89 @@ import ru.myx.ae3.base.BaseObject;
 import ru.myx.ae3.base.BasePrimitiveNumber;
 import ru.myx.ae3.common.Value;
 
-/**
- * @author myx
- * 
- */
+/** @author myx */
 public abstract class AbstractNoInputConsole extends AbstractConsole {
-	private int	newLine	= -1;
 	
-	/**
-	 * To deal with string reader task and uncertainty with CRLF sequence.
-	 * 
-	 * 
+	private int newLine = -1;
+
+	/** To deal with string reader task and uncertainty with CRLF sequence.
+	 *
+	 *
 	 * @param ch
 	 *            '\r' or '\n'
-	 * @return '\r' or '\n' whichever is registered for this session.
-	 */
+	 * @return '\r' or '\n' whichever is registered for this session. */
 	public int checkRegisterNewLine(final int ch) {
+		
 		assert ch == '\r' || ch == '\n' : "ch is none of the valid values: ch=" + ch;
 		return this.newLine == -1
-				? this.newLine = ch
-				: this.newLine;
+			? this.newLine = ch
+			: this.newLine;
 	}
-	
+
 	@Override
 	public final boolean isInteractive() {
+		
 		return false;
 	}
-	
+
 	@Override
 	public boolean isReadable() {
+		
 		return false;
 	}
-	
+
 	@Override
 	public final Value<Boolean> readBoolean(final String title) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
-	
+
 	@Override
 	public final Value<Boolean> readBoolean(final String title, final boolean defaultValue) {
-		return Base.forBoolean( defaultValue );
+		
+		return Base.forBoolean(defaultValue);
 	}
-	
+
 	@Override
-	public final Value<String> readChoose(
-			final String title,
-			final Collection<Map.Entry<String, Object>> selection,
-			final String defaultValue) {
-		// TODO Auto-generated method stub
-		return null;
+	public final Value<String> readChoose(final String title, final Collection<Map.Entry<String, Object>> selection, final String defaultValue) {
+		
+		return Base.forString(defaultValue);
 	}
-	
+
 	@Override
 	public final Value<?> readContinue(final String title) {
+		
 		return BaseObject.TRUE;
 	}
-	
+
 	@Override
 	public final Value<Number> readInteger(final String title) {
+		
 		return BasePrimitiveNumber.NAN;
 	}
-	
+
 	@Override
 	public final Value<Number> readInteger(final String title, final int defaultValue) {
-		return Base.forInteger( defaultValue );
+		
+		return Base.forInteger(defaultValue);
 	}
-	
+
 	@Override
 	public final Value<String> readPassword(final String title) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
-	
+
 	@Override
 	public final Value<String> readString(final String title, final String defaultValue) {
-		return Base.forString( defaultValue );
+		
+		return Base.forString(defaultValue);
 	}
-	
+
 	@Override
 	public final void sendProgress(final String string) {
-		/**
-		 * Do nothing. Non-interactive console is not interacting with supplier
-		 * of its input stream through the output stream.
-		 */
+		
+		/** Do nothing. Non-interactive console is not interacting with supplier of its input stream
+		 * through the output stream. */
 	}
 }
--- java/ru/myx/ae3/console/ConsoleCollector.java
+++ java/ru/myx/ae3/console/ConsoleCollector.java
@@ -1,103 +1,104 @@
 package ru.myx.ae3.console;
 
-import ru.myx.ae3.base.Base;
 import ru.myx.ae3.base.BaseList;
 import ru.myx.ae3.base.BaseObject;
 
-/**
- * 
- * @author myx
- * 
- */
+/** @author myx */
 public class ConsoleCollector extends AbstractNoInputConsole {
-	
-	private BaseList<Object>	events				= null;
-	
-	private ConsoleState		state				= ConsoleState.NORMAL;
-	
-	private boolean				hasNonDefaultOutput	= false;
-	
+
+	private BaseList<Object> events = null;
+
+	private ConsoleState state = ConsoleState.NORMAL;
+
+	private boolean hasNonDefaultOutput = false;
+
 	@Override
 	public void close() {
+		
 		// ignore
 	}
-	
+
 	@Override
 	public boolean fail(final String message) {
+		
 		final ConsoleState state = this.state;
 		try {
 			this.state = ConsoleState.ERROR;
-			this.sendMessage( message );
+			this.sendMessage(message);
 			return false;
 		} finally {
 			this.state = state;
 		}
 	}
-	
+
 	@Override
 	public void flush() {
+		
 		// ignore
 	}
-	
-	/**
-	 * @return
-	 */
+
+	/** @return */
 	public BaseList<?> getCollected() {
+		
 		return this.events;
 	}
-	
-	/**
-	 * 
-	 * @return
-	 */
+
+	/** @return */
 	public boolean getHasNonDefaultOutput() {
+		
 		return this.hasNonDefaultOutput;
 	}
-	
+
 	@Override
 	public void log(final String message) {
+		
 		final ConsoleState state = this.state;
 		try {
 			this.state = ConsoleState.NORMAL;
-			this.sendMessage( message );
+			this.sendMessage(message);
 		} finally {
 			this.state = state;
 		}
 	}
-	
+
 	@Override
 	public void sendMessage(final String message) {
+		
 		if (this.events == null) {
 			this.events = BaseObject.createArray();
 		}
 		if (this.state != ConsoleState.NORMAL) {
 			this.hasNonDefaultOutput = true;
 		}
-		this.events.baseDefaultPush( BaseObject.createObject()//
-				.putAppend( "state", this.state.name() )//
-				.putAppend( "text", message )//
-				);
+		this.events.baseDefaultPush(
+				BaseObject.createObject()//
+						.putAppend("state", this.state.name())//
+						.putAppend("text", message)//
+		);
 	}
-	
+
 	@Override
 	public void sendMessage(final String title, final String text) {
-		this.sendMessage( title + ": " + text );
+		
+		this.sendMessage(title + ": " + text);
 	}
-	
+
 	@Override
 	public void setState(final ConsoleState state) {
+		
 		this.state = state;
 	}
-	
+
 	@Override
 	public void warn(final String message) {
+		
 		final ConsoleState state = this.state;
 		try {
 			this.state = ConsoleState.ATTENTION;
-			this.sendMessage( message );
+			this.sendMessage(message);
 		} finally {
 			this.state = state;
 		}
 	}
-	
+
 }
